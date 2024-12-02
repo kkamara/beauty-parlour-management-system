803acf53ee3f647ba8bcc3d2b259731d4d6bf3fe
kkamara <kamaracomputers@gmail.com>
2024-12-02 17:13:14 +0000

    Add Cart Model relationships

--- app/Models/V1/Cart.php
+++ app/Models/V1/Cart.php
@@ -3,8 +3,10 @@
 namespace App\Models\V1;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Support\Traits\Tappable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Cart extends Model
 {
@@ -21,4 +23,12 @@ class Cart extends Model
         'product_id',
         'quantity',
     ];
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product(): HasOne {
+        return $this->hasOne(Product::class);
+    }
 }
