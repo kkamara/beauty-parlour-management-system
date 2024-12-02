10bbe3ef6de7f99c5c0937de379142b2f63a6e5a
kkamara <kamaracomputers@gmail.com>
2024-12-02 17:21:15 +0000

    Update migration: create_carts_table

--- database/migrations/v1/2024_12_02_144504_create_carts_table.php
+++ database/migrations/v1/2024_12_02_144504_create_carts_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger("user_id");
             $table->unsignedBigInteger("product_id");
             $table->tinyInteger("quantity")->default(1);
             $table->timestamps();
 
+            $table->foreign("user_id")
+                ->references("id")
+                ->on("users");
             $table->foreign("product_id")
                 ->references("id")
                 ->on("products");
