847f032c1649f7d6f2e1f2985ab5a4df88aaf2ea
kkamara <kamaracomputers@gmail.com>
2024-12-18 15:32:46 +0000

    Only logged in users can access order result page

--- resources/js/components/pages/checkout/CheckoutResultComponent.jsx
+++ resources/js/components/pages/checkout/CheckoutResultComponent.jsx
@@ -8,12 +8,15 @@ import Message from "./MessageComponent"
 export default function CheckoutResultComponent() {
   const navigate = useNavigate()
   const dispatch = useDispatch()
-  const orderState = useSelector(state => state.order)
+  const state = useSelector(state => ({
+    order: state.order,
+    auth: state.auth,
+  }))
   const [message, setMessage] = useState("")
   const [title, setTitle] = useState("")
 
   useEffect(() => {
-    const query = new URLSearchParams(window.location.search);
+    const query = new URLSearchParams(window.location.search)
 
     if (query.get("orderId")) {
       dispatch(getOrder(query.get("orderId")))
@@ -32,16 +35,22 @@ export default function CheckoutResultComponent() {
       )
       setTitle("Order Cancelled")
     }
-  }, []);
+  }, [])
 
   useEffect(() => {
-    if (false === orderState.loading && null !== orderState.error) {
+    if (false === state.order.loading && null !== state.order.error) {
       return navigate("/404")
     }
-  }, [orderState,])
+  }, [state.order,])
+
+  useEffect(() => {
+    if (false === state.auth.loading && null !== state.auth.error) {
+      return navigate("/user/login")
+    }
+  }, [state.auth])
 
   const renderPreferredSchedules = () => {
-    const preferredSchedules = orderState.data.data.preferredSchedules
+    const preferredSchedules = state.order.data.data.preferredSchedules
     if (0 === preferredSchedules.length) {
       return null
     }
@@ -54,7 +63,7 @@ export default function CheckoutResultComponent() {
     ))}</p>
   }
 
-  if (orderState.loading) {
+  if (state.order.loading) {
     return <div className='container login-container text-center'>
       <Helmet>
         <title>Checkout Order - {import.meta.env.VITE_APP_NAME}</title>
@@ -71,7 +80,7 @@ export default function CheckoutResultComponent() {
       <Message message={message} />
       <section className="col-md-10 offset-md-1 text-center">
         <h4>
-          Order {orderState.data.data.id} for {orderState.data.data.orderProducts[0].name} at {orderState.data.data.formattedPrice}
+          Order {state.order.data.data.id} for {state.order.data.data.orderProducts[0].name} at {state.order.data.data.formattedPrice}
         </h4>
         {renderPreferredSchedules()}
       </section>
