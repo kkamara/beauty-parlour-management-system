9297d16e5a6259dc24065c709c6251f0f1cbf120
kkamara <kamaracomputers@gmail.com>
2024-12-21 17:59:10 +0000

    Update icon for Cart resource in Filament admin panel navbar

--- app/Filament/Resources/V1/CartResource.php
+++ app/Filament/Resources/V1/CartResource.php
@@ -21,7 +21,7 @@ class CartResource extends Resource
 {
     protected static ?string $model = Cart::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-shopping-cart';
 
     public static function form(Form $form): Form
     {
